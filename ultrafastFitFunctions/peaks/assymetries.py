8037cc44e28738b5418b02406a0ffbc1cc5be9a2
EmCeBeh <martin.b@robothek.de>
2022-03-16 13:12:48 +0100

    assymetries altered

--- ultrafastFitFunctions/peaks/assymetries.py
+++ ultrafastFitFunctions/peaks/assymetries.py
@@ -4,8 +4,8 @@ from scipy.special import erf, erfc
 
 def gaussAssymetry(x, mu, sig, alpha):
     model = 1+erf((alpha*(x-mu))/(sig*sqrt(2)))
-    return model
+    return model/2
 
 def lorentzAssymetry(x, mu, sig, alpha):
     model = 1+arctan(alpha*(x-mu)/sig)/(pi/2)
-    return model
+    return model/2
