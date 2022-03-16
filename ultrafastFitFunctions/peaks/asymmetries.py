3b287a70bde94ba6a0dff137a68c7fb741d1abdb
EmCeBeh <martin.b@robothek.de>
2022-03-16 13:24:12 +0100

    fixed awful spelling mistake

--- ultrafastFitFunctions/peaks/asymmetries.py
+++ ultrafastFitFunctions/peaks/asymmetries.py
@@ -2,10 +2,10 @@ from numpy import *  #pi, exp, sqrt, etc.
 from scipy.special import erf, erfc
 
 
-def gaussAssymetry(x, mu, sig, alpha):
+def gaussAsymmetry(x, mu, sig, alpha):
     model = 1+erf((alpha*(x-mu))/(sig*sqrt(2)))
     return model/2
 
-def lorentzAssymetry(x, mu, sig, alpha):
+def lorentzAsymmetry(x, mu, sig, alpha):
     model = 1+arctan(alpha*(x-mu)/sig)/(pi/2)
     return model/2
